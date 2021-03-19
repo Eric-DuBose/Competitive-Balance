0f3ca9072aa580bb7728adaad65477367caf07a4
Eric DuBose <edubose@wgu.edu>
2021-03-18 23:10:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,9 +55,14 @@ def comp_balance(league):
 sbc_comp_bal = comp_balance('SBC')
 sbc_comp_bal = sbc_comp_bal[sbc_comp_bal['Playoffs'] == 0]
 
-with st.beta_container():
-    st.write("Revenue Eligible")
+
+col1, col2 = st.beta_columns(2)
+with col1:
+    st.header("Revenue Eligible")
     st.table(sbc_comp_bal[['Team','Total Revenue']].sort_values('Total Revenue', ascending = True).head(10))
 
-st.write("Market Eligible")
-st.table(sbc_comp_bal[['Team','Market Size']].sort_values('Market Size', ascending = True).head(10))
+
+
+with col2:
+    st.header("Market Eligible")
+    st.table(sbc_comp_bal[['Team','Market Size']].sort_values('Market Size', ascending = True).head(10))
